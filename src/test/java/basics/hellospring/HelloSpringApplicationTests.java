b39816efec1256300be24928af2b2047f6029c60
parksay_desktop <hellobigworld.parksay@gmail.com>
2023-07-27 17:48:58 +0900

    initial commit

---
+++ src/test/java/basics/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package basics.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
